8482529d428fb1b673f4dc56221ca020fa5ae3e0
pedro-andrade <prodrigu@macbook-de-teresa-neto.local>
2014-06-01 11:09:56 +0200

    Added official flags pngs

---
+++ tools/get_flags.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+cd ../worldcup2014/static/img/flags
+curl http://img.fifa.com/images/flags/3/cro.png -o crotia.png
+curl http://img.fifa.com/images/flags/3/chi.png -o chile.png
+curl http://img.fifa.com/images/flags/3/aus.png -o australia.png
+curl http://img.fifa.com/images/flags/3/col.png -o colombia.png
+curl http://img.fifa.com/images/flags/3/gre.png -o greece.png
+curl http://img.fifa.com/images/flags/3/uru.png -o uruguay.png
+curl http://img.fifa.com/images/flags/3/crc.png -o costa-rica.png
+curl http://img.fifa.com/images/flags/3/eng.png -o england.png
+curl http://img.fifa.com/images/flags/3/ita.png -o italy.png
+curl http://img.fifa.com/images/flags/3/civ.png -o cote-d-ivoire.png
+curl http://img.fifa.com/images/flags/3/jpn.png -o japan.png
+curl http://img.fifa.com/images/flags/3/sui.png -o switzerland.png
+curl http://img.fifa.com/images/flags/3/ecu.png -o ecuador.png
+curl http://img.fifa.com/images/flags/3/fra.png -o france.png
+curl http://img.fifa.com/images/flags/3/hon.png -o honduras.png
+curl http://img.fifa.com/images/flags/3/arg.png -o argentina.png
+curl http://img.fifa.com/images/flags/3/bih.png -o bosnia-herzegovina.png
+curl http://img.fifa.com/images/flags/3/ger.png -o deutschland.png
+curl http://img.fifa.com/images/flags/3/por.png -o portugal.png
+curl http://img.fifa.com/images/flags/3/irn.png -o iran.png
+curl http://img.fifa.com/images/flags/3/nga.png -o nigeria.png
+curl http://img.fifa.com/images/flags/3/gha.png -o ghana.png
+curl http://img.fifa.com/images/flags/3/usa.png -o united-states.png
+curl http://img.fifa.com/images/flags/3/bel.png -o belgium.png
+curl http://img.fifa.com/images/flags/3/alg.png -o algeria.png
+curl http://img.fifa.com/images/flags/3/bra.png -o brazil.png
+curl http://img.fifa.com/images/flags/3/mex.png -o mexico.png
+curl http://img.fifa.com/images/flags/3/rus.png -o russia.png
+curl http://img.fifa.com/images/flags/3/kor.png -o south-korea.png
+curl http://img.fifa.com/images/flags/3/ned.png -o netherlands.png
+curl http://img.fifa.com/images/flags/3/esp.png -o espana.png
+curl http://img.fifa.com/images/flags/3/cmr.png -o cameroon.png
