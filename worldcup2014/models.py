51dd4bf5753da5ebd11db40cf73da514e6107e7e
TeresaNeto <tneto@10.0.1.5>
2014-06-02 21:45:52 +0200

    Added foreign key between winner and team

--- worldcup2014/models.py
+++ worldcup2014/models.py
@@ -17,24 +17,24 @@ class Match(models.Model):
     teamA = models.ForeignKey(Team, related_name='teamA', blank=False)
     teamB = models.ForeignKey(Team, related_name='teamB', blank=False)
     matchtime = models.DateTimeField('Date')
-    winner = models.CharField(max_length=200, blank=True)
+    winner = models.ForeignKey(Team, related_name='winner', blank=True)
     score = models.CharField(max_length=200, blank=True)
 
     def __unicode__(self):
         return "%s - %s " % (self.teamA, self.teamB)
 
 class MatchStriker(models.Model):
-	match = models.ForeignKey(Match, blank=False)
-	striker = models.ForeignKey(Player, blank=False)
-	
-	def __unicode__(self):
-		return "%d - %d " % (self.match, self.striker)
-	
+    match = models.ForeignKey(Match, blank=False)
+    striker = models.ForeignKey(Player, blank=False)
+    
+    def __unicode__(self):
+        return "%d - %d " % (self.match, self.striker)
+
 class Vote(models.Model):
     match = models.ForeignKey(Match)
     user = models.CharField(max_length=200, blank=False)
     striker = models.ForeignKey(Player, related_name='strikerVote', blank=False)
-    winner = models.CharField(max_length=200, blank=False)
+    winner = models.ForeignKey(Team, related_name='winnerVote', blank=False)
     score = models.CharField(max_length=200, blank=False)
     
     def __unicode__(self):
