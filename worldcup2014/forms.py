5b586e516e2ae9f933b8ca4deee10032b7a872af
TeresaNeto <tneto@10.0.1.5>
2014-06-04 23:44:32 +0200

    excluded disabled fields from the vote form (django bug - disabled fields get empty when a validation fails)

--- worldcup2014/forms.py
+++ worldcup2014/forms.py
@@ -1,19 +1,17 @@
 from django import forms
-from django.utils.safestring import mark_safe
 
-from worldcup2014.models import Vote, Player, Team, Match
-from django.core.exceptions import ValidationError
+from worldcup2014.models import Vote, Player, Team
 
+class NoEmptyChoiceField(forms.ModelChoiceField): 
 
+    def __init__(self, *args, **kwargs): 
+        super(NoEmptyChoiceField, self).__init__(empty_label=None, *args, **kwargs) 
+          
 class VoteForm(forms.ModelForm):
     class Meta:
         model = Vote
-        fields = ('match', 'user', 'striker', 'winner', 'score')
-##icon = ChoiceField(choices=Player._meta.get_field_by_name('icon')[0].choices, 
- ##                  widget=IconPicker)
-
-##    plan = forms.ModelChoiceField(queryset = Plan.objects.none()) 
-      #  striker = forms.ModelChoiceField(queryset=None, empty_label=None)
+        fields = ('striker', 'winner', 'score')
+        exclude = ('match', 'user')
         
     def __init__(self, *args, **kwargs):
         super(VoteForm, self).__init__(*args, **kwargs)
@@ -22,16 +20,6 @@ class VoteForm(forms.ModelForm):
         
         self.fields['striker'].queryset = Player.objects.filter(team=instance.match.teamA) | Player.objects.filter(team=instance.match.teamB)
         self.fields['winner'].queryset = Team.objects.filter(name=instance.match.teamA.name) | Team.objects.filter(name=instance.match.teamB.name) | Team.objects.filter(name='-')
-        
-        if instance and instance.match:
-            self.fields['match'].label = u'Match'
-            self.fields['match'].required = False
-            self.fields['match'].widget.attrs['disabled'] = True
-        if instance and instance.user:
-            self.fields['user'].label = u'User'
-            self.fields['user'].required = False
-            self.fields['user'].widget.attrs['disabled'] = True
-
 
         self.fields['striker'].label = u'Striker'
         self.fields['striker'].required = True
@@ -42,17 +30,4 @@ class VoteForm(forms.ModelForm):
         self.fields['score'].label = u'Score'
         self.fields['score'].required = True
         self.fields['score'].widget = forms.TextInput()
-
-
-    def clean_match(self):
-        if self.instance and self.instance.match:
-            return self.instance.match
-        else:
-            return self.cleaned_data['match']        
-
-    def clean_user(self):
-        if self.instance and self.instance.user:
-            return self.instance.user
-        else:
-            return self.cleaned_data['user']
                         
