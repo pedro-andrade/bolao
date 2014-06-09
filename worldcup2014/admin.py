e7aad76d49021716d4a1ed0fed81cf9cb3896891
TeresaNeto <tneto@10.0.1.5>
2014-06-09 22:11:57 +0200

    Added extra vote

--- worldcup2014/admin.py
+++ worldcup2014/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from worldcup2014.models import Team, Player, Match, MatchStriker, Vote
+from worldcup2014.models import Team, Player, Match, MatchStriker, Vote, ExtraVote
 
 admin.site.register(Team)
 admin.site.register(Player)
 admin.site.register(Match)
 admin.site.register(MatchStriker)
 admin.site.register(Vote)
+admin.site.register(ExtraVote)
 
 
