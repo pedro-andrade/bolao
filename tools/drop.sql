f9c6cf316979532bf047ac6e3ed6a8d80a770f9c
TeresaNeto <tneto@10.0.1.5>
2014-06-09 18:21:24 +0200

    commit drop.sql

---
+++ tools/drop.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+DROP TABLE worldcup2014_match;
+DROP TABLE worldcup2014_matchstriker;
+DROP TABLE worldcup2014_vote;
+DROP TABLE worldcup2014_team;
+DROP TABLE worldcup2014_player;
+
+COMMIT;
