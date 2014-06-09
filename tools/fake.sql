b5ed5ab233b55e05a28a7a4d01b38de8e48fa8a8
TeresaNeto <tneto@10.0.1.5>
2014-06-09 18:19:53 +0200

    added drop sql script

--- tools/fake.sql
+++ tools/fake.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
-INSERT INTO worldcup2014_matchstriker VALUES (1, 1, 1);
+INSERT INTO worldcup2014_matchstriker VALUES (1, 1, 111);
 UPDATE worldcup2014_match SET winner_id = 5 WHERE id = 1;
 UPDATE worldcup2014_match SET score = '2-0' WHERE id = 1;
 
-INSERT INTO worldcup2014_matchstriker VALUES (2, 2, 3);
+INSERT INTO worldcup2014_matchstriker VALUES (2, 2, 651);
 UPDATE worldcup2014_match SET winner_id = 26 WHERE id = 2;
 UPDATE worldcup2014_match SET score = '1-1' WHERE id = 2;
 
